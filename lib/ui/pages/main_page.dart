9f179d47cf48ba99ab00ba230033ecbd90f2dbe1
Bayu Hermawan <bayuhermawan957@gmail.com>
2022-02-12 09:06:29 +0800

    first commit

---
+++ lib/ui/pages/main_page.dart
@@ -0,0 +1,85 @@
+import 'package:airplane/cubit/page_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../shared/theme.dart';
+import '../widgets/custom_bottom_navigation_item.dart';
+import 'home_page.dart';
+import 'setting_page.dart';
+import 'transaction_page.dart';
+import 'wallet_page.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget buildContent(int currentIndex) {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+        case 1:
+          return TransactionPage();
+        case 2:
+          return WalletPage();
+        case 3:
+          return SettingPage();
+        default:
+          return HomePage();
+      }
+    }
+
+    Widget customBottomNavigation() {
+      return Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          width: double.infinity,
+          height: 60,
+          margin: EdgeInsets.only(
+            bottom: 20,
+            left: defaultMargin,
+            right: defaultMargin,
+          ),
+          decoration: BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.circular(defaultRadius),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CustomBottomNavigationItem(
+                index: 0,
+                imageUrl: 'assets/icon_home.png',
+              ),
+              CustomBottomNavigationItem(
+                index: 1,
+                imageUrl: 'assets/icon_booking.png',
+              ),
+              CustomBottomNavigationItem(
+                index: 2,
+                imageUrl: 'assets/icon_card.png',
+              ),
+              CustomBottomNavigationItem(
+                index: 3,
+                imageUrl: 'assets/icon_settings.png',
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return BlocBuilder<PageCubit, int>(
+      builder: (context, currentIndex) {
+        return Scaffold(
+          backgroundColor: kBackgroundColor,
+          body: Stack(
+            children: [
+              buildContent(currentIndex),
+              customBottomNavigation(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
